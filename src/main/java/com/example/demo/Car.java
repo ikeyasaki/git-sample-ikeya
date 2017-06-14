eae28d9ef97b3143553ee88d4ddaa95df5f1877c
kazuki.hirayama <kazuki.hirayama@rakus.co.jp>
2017-06-14 14:01:18 +0900

    Carクラスを編集.

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -1,5 +1,32 @@
 package com.example.demo;
 
 public class Car {
+	/*
+	 * ここから平山が編集
+	 */
+	private String name;
+	private Integer speed;
 
+	public Car() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	/*
+	 * ここまで編集
+	 */
 }
