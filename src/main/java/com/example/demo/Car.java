d2d5e39c3c4c6be414b9bbc7d867e1ede135f1c7
saki.ikeya <saki.ikeya@rakus.co.jp>
2017-06-14 14:04:06 +0900

    Carクラスを編集しました。

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -1,5 +1,16 @@
 package com.example.demo;
 
 public class Car {
+	private int speed;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	
 
 }
